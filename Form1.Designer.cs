45cbd058d862e1fc95930bc1752454faea8d7be4
leratimofeeva <leratimofeeva129@gmail.com>
2024-05-15 11:50:46 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.RegionName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2009 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2010 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2011 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2012 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2013 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2014 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2015 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2016 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2017 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2018 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2019 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2020 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2021 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2022 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.N2023 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pizm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1262, 601);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1254, 575);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Загрузка данных";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1115, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RegionName,
+            this.N2009,
+            this.N2010,
+            this.N2011,
+            this.N2012,
+            this.N2013,
+            this.N2014,
+            this.N2015,
+            this.N2016,
+            this.N2017,
+            this.N2018,
+            this.N2019,
+            this.N2020,
+            this.N2021,
+            this.N2022,
+            this.N2023,
+            this.Pizm});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1083, 539);
+            this.dataGridView1.TabIndex = 0;
+            //this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // RegionName
+            // 
+            this.RegionName.HeaderText = "RegionName";
+            this.RegionName.Name = "RegionName";
+            // 
+            // N2009
+            // 
+            this.N2009.HeaderText = "2009";
+            this.N2009.Name = "N2009";
+            // 
+            // N2010
+            // 
+            this.N2010.HeaderText = "2010";
+            this.N2010.Name = "N2010";
+            // 
+            // N2011
+            // 
+            this.N2011.HeaderText = "2011";
+            this.N2011.Name = "N2011";
+            // 
+            // N2012
+            // 
+            this.N2012.HeaderText = "2012";
+            this.N2012.Name = "N2012";
+            // 
+            // N2013
+            // 
+            this.N2013.HeaderText = "2013";
+            this.N2013.Name = "N2013";
+            // 
+            // N2014
+            // 
+            this.N2014.HeaderText = "2014";
+            this.N2014.Name = "N2014";
+            // 
+            // N2015
+            // 
+            this.N2015.HeaderText = "2015";
+            this.N2015.Name = "N2015";
+            // 
+            // N2016
+            // 
+            this.N2016.HeaderText = "2016";
+            this.N2016.Name = "N2016";
+            // 
+            // N2017
+            // 
+            this.N2017.HeaderText = "2017";
+            this.N2017.Name = "N2017";
+            // 
+            // N2018
+            // 
+            this.N2018.HeaderText = "2018";
+            this.N2018.Name = "N2018";
+            // 
+            // N2019
+            // 
+            this.N2019.HeaderText = "2019";
+            this.N2019.Name = "N2019";
+            // 
+            // N2020
+            // 
+            this.N2020.HeaderText = "2020";
+            this.N2020.Name = "N2020";
+            // 
+            // N2021
+            // 
+            this.N2021.HeaderText = "2021";
+            this.N2021.Name = "N2021";
+            // 
+            // N2022
+            // 
+            this.N2022.HeaderText = "2022";
+            this.N2022.Name = "N2022";
+            // 
+            // N2023
+            // 
+            this.N2023.HeaderText = "2023";
+            this.N2023.Name = "N2023";
+            // 
+            // Pizm
+            // 
+            this.Pizm.HeaderText = "Pizm";
+            this.Pizm.Name = "Pizm";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1254, 575);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Графики";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1126, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Построить графики";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(19, 22);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1088, 547);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.numericUpDown2);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.numericUpDown1);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.chart2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1254, 575);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Экстраполяция";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(614, 203);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(614, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "число n (период)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(614, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ввести количество N лет";
+            //this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(614, 133);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(614, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Найти и экстраполировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(18, 21);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(431, 346);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 616);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RegionName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2009;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2010;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2011;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2012;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2013;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2014;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2015;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2016;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2017;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2018;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2019;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2020;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2021;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2022;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N2023;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pizm;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_3
+{
+    public partial class Form1 : Form
+    {
+        ListRegion listRegion = new ListRegion(); // создание объекта список регионов
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //загрузка данных из Excel
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ListRegion listRegion = new ListRegion();
+            listRegion.LoadFromExcel(); // загрузка из экселя
+
+            // очищение dataGridView1
+            dataGridView1.Rows.Clear();
+
+            foreach (var LR in listRegion.regions)
+            {
+                //dataGridView1.Rows.Add(LR); // столбцы называются так же как и свойства
+                dataGridView1.Rows.Add(LR.RegionName, LR.N2009, LR.N2010, LR.N2011, LR.N2012, LR.N2013, LR.N2014, LR.N2015, LR.N2016, LR.N2017, LR.N2018, LR.N2019, LR.N2020, LR.N2021, LR.N2022, LR.N2023, LR.Pizm);
+            }
+        }
+
+        //построить графики по данным таблицы
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            Random rnd = new Random();
+            //chart1.Series[2].Points.Clear(); // очищение точек
+
+            //MessageBox.Show("Число точек должно быть не меньше 4");
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            //for (int i = 1; i < 86; i++)
+            {
+                int r = rnd.Next(0, 255);
+                int g = rnd.Next(0, 255);
+                int b = rnd.Next(0, 255);
+
+                chart1.Series.Add("Series" + i.ToString());
+                chart1.Series["Series" + i.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series["Series" + i.ToString()].Color = Color.FromArgb(255, r, g, b);
+
+                chart1.Series["Series" + i.ToString()].LegendText = dataGridView1.Rows[i].Cells[0].Value.ToString(); //"Название Series"
+                                                                                                                     //MessageBox.Show("Назавание " + dataGridView1.Rows[i].Cells[0].Value);
+
+
+
+                for (int k = 1; k < 16; k++)
+                {
+                    int x = 2008 + k;
+                    int y = Convert.ToInt32(dataGridView1.Rows[i].Cells[k].Value);
+                    chart1.Series["Series" + i.ToString()].Points.AddXY(x, y);
+                    // тип графика или в свойствах
+                    chart1.Series["Series" + i.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                    //chart1.Series["Series" + i.ToString()].Points.AddXY(x, y);
+                }
+            }
+
+            // Создание оси X
+            chart1.ChartAreas[0].AxisX.Minimum = 2009;
+            chart1.ChartAreas[0].AxisX.Maximum = 2023; // Диапазон значений X от 2008 до 2023;
+            chart1.ChartAreas[0].AxisX.Interval = 1; // Шаг значения X
+
+            //// Создание оси Y
+            chart1.ChartAreas[0].AxisY.Minimum = 0; // Минимальное значение Y
+            //chart1.ChartAreas[0].AxisY.Maximum = 12000000; // Максимальное значение Y
+            //chart1.ChartAreas[0].AxisY.Interval = 1; // Шаг значения Y
+        }
+
+        // экстраполяция
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int NYears_ = Convert.ToInt32(numericUpDown1.Value);  // количество лет для экстраполяции
+            int kol = Convert.ToInt32(numericUpDown2.Value);   // интервал времени
+
+            List<Region> copy = new List<Region>();
+
+            copy = listRegion.copy(); // поиск двух нужных строк
+
+            //dataGridView2.Rows.Clear();
+
+            //foreach (var LR in copy)
+            //{
+            //    //dataGridView1.Rows.Add(LR); // столбцы называются так же как и свойства
+            //    dataGridView2.Rows.Add(LR.RegionName, LR.N2009, LR.N2010, LR.N2011, LR.N2012, LR.N2013, LR.N2014,
+            //        LR.N2015, LR.N2016, LR.N2017, LR.N2018, LR.N2019, LR.N2020, LR.N2021, LR.N2022, LR.N2023, LR.Pizm);
+            //}
+
+
+            chart2.Series.Clear();
+            Random rnd = new Random();
+
+            int i = 0;
+            //MessageBox.Show("Число точек должно быть не меньше 4");
+
+            foreach (var LR in copy) // построение графика этих строк
+            {
+                // массив для экстраполяции
+
+                List<double> massEkstp = new List<double>();
+                i++;
+                int r = rnd.Next(0, 255);
+                int g = rnd.Next(0, 255);
+                int b = rnd.Next(0, 255);
+
+                chart2.Series.Add("Series" + i.ToString());
+                //chart2.Series.Add("Series" + i.ToString());
+                chart2.Series["Series" + i.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart2.Series["Series" + i.ToString()].Color = Color.FromArgb(255, r, g, b);
+
+                chart2.Series["Series" + i.ToString()].LegendText = LR.RegionName; //"Название Series"
+
+                // другие цвета  график экстраполяции
+                r = rnd.Next(0, 255);
+                g = rnd.Next(0, 255);
+                b = rnd.Next(0, 255);
+                chart2.Series.Add("Series" + (i + 2).ToString());
+                chart2.Series["Series" + (i + 2).ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart2.Series["Series" + (i + 2).ToString()].Color = Color.FromArgb(255, r, g, b);
+                chart2.Series["Series" + (i + 2).ToString()].LegendText = LR.RegionName + " экстрапол";
+
+                //MessageBox.Show("Назавание " + dataGridView1.Rows[i].Cells[0].Value);
+                //massEkstp.Add((2010, LR.N2010));
+                // вывод текущих точек на график и заполнение массива для экстраполяции
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2009, LR.N2009); massEkstp.Add(LR.N2009);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2010, LR.N2010); massEkstp.Add(LR.N2010);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2011, LR.N2011); massEkstp.Add(LR.N2011);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2012, LR.N2012); massEkstp.Add(LR.N2012);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2013, LR.N2013); massEkstp.Add(LR.N2013);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2014, LR.N2014); massEkstp.Add(LR.N2014);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2015, LR.N2015); massEkstp.Add(LR.N2015);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2016, LR.N2016); massEkstp.Add(LR.N2016);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2017, LR.N2017); massEkstp.Add(LR.N2017);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2018, LR.N2018); massEkstp.Add(LR.N2018);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2019, LR.N2019); massEkstp.Add(LR.N2019);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2020, LR.N2020); massEkstp.Add(LR.N2020);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2021, LR.N2021); massEkstp.Add(LR.N2021);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2022, LR.N2022); massEkstp.Add(LR.N2022);
+                chart2.Series["Series" + i.ToString()].Points.AddXY(2023, LR.N2023); massEkstp.Add(LR.N2023);
+                // последнюю точку добавим на график экстраполяции
+                chart2.Series["Series" + (i + 2).ToString()].Points.AddXY(2023, LR.N2023); massEkstp.Add(LR.N2023);
+                // тип графика или в свойствах
+                //chart1.Series["Series" + i.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+                // Экстраполяция
+                int kolmass = massEkstp.Count;
+                while (kol < kolmass) // удаляем ненужные сначала элементы для экстраполяции на необходимое количество лет
+                {
+                    massEkstp.RemoveAt(0);// уменьшаем количество элементов в массиве
+                    kolmass--; // уменьшаем количество элементов в массиве
+                }
+
+
+                for (int k = 1; k <= NYears_; k++) // цикл по количеству лет
+                {
+                    int x = 2023 + k; // год
+                    double y = Convert.ToInt32(listRegion.extrapol(massEkstp, kol)); // численность,  получение по экстраполяции
+
+                    // удалить первый элемент
+                    massEkstp.RemoveAt(0);
+                    // добавить новый элемент в массив экстраполяции
+                    massEkstp.Add(y);
+
+                    chart2.Series["Series" + (i + 2).ToString()].Points.AddXY(x, y);
+
+                    // тип графика или в свойствах
+                    //chart2.Series["Series" + i.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+
+                }
+            }
+
+            // Создание оси X
+            chart2.ChartAreas[0].AxisX.Minimum = 2009;
+            chart2.ChartAreas[0].AxisX.Maximum = 2023 + NYears_; // Диапазон значений X от 2008 до 2023 + NYears_;
+            chart2.ChartAreas[0].AxisX.Interval = 1; // Шаг значения X
+
+            //// Создание оси Y
+            chart2.ChartAreas[0].AxisY.Minimum = 0; // Минимальное значение 
+        }
+    }
+}
